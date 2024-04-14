cbedfee0619e3c600a8c883e078a869e070a8988
Ninalalov Murad <Murad267@yandex.ru>
2024-04-14 10:36:31 +0300

    6.1 second

--- lesson_6.1/market_test.py
+++ lesson_6.1/market_test.py
@@ -3,8 +3,6 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import pytest
-from time import sleep
-
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
--- lesson_6.1/test_filling.py
+++ lesson_6.1/test_filling.py
@@ -6,10 +6,10 @@ bad_selectors_list = ['zip-code']
     
 def test_bad_filling_the_form():
     for i in bad_elems_id:
-        assert i in bad_selectors_list
+        assert i in bad_selectors_list and len(bad_elems_id) == 1
      
            
 def test_good_filling_the_form():
     for i in good_elems_id:
-        assert i in good_selectors_list
+        assert i in good_selectors_list and len(good_elems_id) == 9
         
